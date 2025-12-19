d8aefc61be3ef988fa97aa7b73cbf8c5aa74e741
Chris Jones <cjones@labrnth.xyz>
2025-12-18 19:44:09 -0800

    Change in colors.fg to #ECEFF1

--- colors/lairetam.lua
+++ colors/lairetam.lua
@@ -1,35 +1,35 @@
 vim.cmd("hi clear")
 if vim.fn.exists("syntax_on") then
-	vim.cmd("syntax reset")
+  vim.cmd("syntax reset")
 end
 
 vim.g.colors_name = "lairetam"
 
 -- Custom color palette
 local colors = {
-	bg = "#263238",
-	fg = "#CDD3DE",
-	cursorline_bg = "#37474F",
-	visual_bg = "#455A64",
-	black = "#263238",
-	darker_black = "#1E282C",
-	red = "#E57373",
-	baby_pink = "#F06292",
-	pink = "#F06292",
-	green = "#81C784",
-	vibrant_green = "#B9F6CA",
-	nord_blue = "#1E88E5",
-	blue = "#64B5F6",
-	yellow = "#FFF176",
-	sun = "#FFD54F",
-	purple = "#BA68C8",
-	dark_purple = "#9575CD",
-	teal = "#4DB6AC",
-	orange = "#FFB74D",
-	cyan = "#4DD0E1",
-	light_grey = "#78909C",
-	grey = "#607D8B",
-	dark_grey = "#546E7A",
+  bg = "#263238",
+  fg = "#ECEFF1",
+  cursorline_bg = "#37474F",
+  visual_bg = "#455A64",
+  black = "#263238",
+  darker_black = "#1E282C",
+  red = "#E57373",
+  baby_pink = "#F06292",
+  pink = "#F06292",
+  green = "#81C784",
+  vibrant_green = "#B9F6CA",
+  nord_blue = "#1E88E5",
+  blue = "#64B5F6",
+  yellow = "#FFF176",
+  sun = "#FFD54F",
+  purple = "#BA68C8",
+  dark_purple = "#9575CD",
+  teal = "#4DB6AC",
+  orange = "#FFB74D",
+  cyan = "#4DD0E1",
+  light_grey = "#78909C",
+  grey = "#607D8B",
+  dark_grey = "#546E7A",
 }
 
 -- Apply basic highlights
